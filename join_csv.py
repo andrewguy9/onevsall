b45d13a2f839ba3f6c329f875032c3680b3136ee
Andrew Thomson <athomsonguy@gmail.com>
2014-01-09 17:31:56 -0800

    Big updates to join_csv.py. Now table is a class, but join isn't done.

--- join_csv.py
+++ join_csv.py
@@ -4,6 +4,8 @@ import argparse
 from argparse import ArgumentParser
 from csv import reader, writer
 from sys import stdout
+from tempfile import NamedTemporaryFile
+from os import rename
 
 def join_arg(string):
     try:
@@ -18,29 +20,47 @@ def join_arg(string):
 
 # fileA fileB keyA keyB fileC keyC keyD
 parser = ArgumentParser()
+parser.add_argument('--output', help='file to write to')
 parser.add_argument("base", help="table to start with")
 parser.add_argument("join_arg", type=join_arg, nargs='*', help="file.csv:left_key_index:right_key_index")
 
-def create_table(f):
-    r = reader(f)
-    table = []
-    for record in r:
-        table.append(record)
-    return table
+class table:
+    def __init__(self, path):
+        self.path = path
 
-def create_index(table, key_index):
-    index = {}
-    for record in table:
-        key = record[key_index]
-        if key in index:
-            raise ValueError("Key %s is not unique" % key)
-        index[key] = record
-    return index
+    def cursor(self):
+        with open(self.path, 'r') as fd:
+            r = reader(fd)
+            for record in r:
+                yield record
+
+    def dict_cursor(self):
+        with open(self.path, 'r') as fd:
+            r = reader(fd)
+            headers = r.next()
+            index_field = list(enumerate(headers))
+            for row in r:
+                dict_row = {field: row[index] for (index, field) in index_field}
+                yield dict_row
+
+    def index(self, key_index):
+        index = {}
+        for record in self.cursor():
+            key = record[key_index]
+            if key in index:
+                raise ValueError("Key %s is not unique" % key)
+            index[key] = record
+        return index
+
+    def write(self, f):
+        out = writer(f)
+        for record in self.cursor():
+            out.writerow(map(str, record))
 
 def left_join(left, left_key, right_index, right_key):
     accum = []
     # print "&&& running key %d &&&" % left_key
-    for record in left:
+    for record in left.cursor():
         row = []
         row += record
         key = record[left_key]
@@ -53,29 +73,26 @@ def left_join(left, left_key, right_index, right_key):
         accum.extend([row])
     return accum
 
-def print_table(table, f):
-    out = writer(f)
-    # print table
-    for record in table:
-        # print record
-        out.writerow(map(str, record))
-
 def main():
     args = parser.parse_args()
-    with open(args.base, 'r') as accum_f:
-        accum = create_table(accum_f)
+    accum = table(args.base)
+    if args.output:
+        out_h = NamedTemporaryFile()
+    else:
+        out_h = stdout
     # print "***Start table***"
-    # print_table(accum, stdout)
+    # accum.write(stdout)
     for (table_name, left_index, right_index) in args.join_arg:
-        with open(table_name, 'r') as f:
-            table = create_table(f)
-            # print "***right table table***"
-            # print_table(table, stdout)
-            index = create_index(table, right_index)
-            accum = left_join(accum, left_index, index, right_index)
-            # print "***joined table***"
-            # print_table(accum, stdout)
-    print_table(accum, stdout)
+        right = table(table_name)
+        # print "***right table ***"
+        # right.write(stdout)
+        index = right.index(right_index)
+        accum = left_join(accum, left_index, index, right_index)  # TODO FIX JOIN.
+        # print "***joined table***"
+        # accum.write(stdout)
+    accum.write(stdout)
+    if out_h != stdout:
+        rename(out_h.name, args.output)
 
 if __name__ == '__main__':
     main()
